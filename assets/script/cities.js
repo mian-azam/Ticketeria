346d820d995b934c7fcef178a970abdcbe12bcdf
mian-azam <tarbelite04@gmail.com>
2023-05-31 15:24:50 -0500

    Cities & Movies

---
+++ assets/script/cities.js
@@ -0,0 +1,27 @@
+export default{  "cities": [
+    {
+      "name": "Alberta"
+    },
+    {
+      "name": "Halifax"
+    },
+    {
+      "name": "Ottawa"
+    },
+    {
+      "name": "Quebec"
+    },
+    {
+      "name": "Saskatchewan"
+    },
+    {
+      "name": "Vancouver"
+    },
+    {
+      "name": "Victoria"
+    },
+    {
+      "name": "Winnipeg"
+    }
+  ]
+}
